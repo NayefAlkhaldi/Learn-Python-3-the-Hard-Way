37a16765bbfb90f688373984fd7181a325ad0c2a
NayefAlkhaldi <100409462+NayefAlkhaldi@users.noreply.github.com>
2022-06-26 08:47:05 +0300

    Done!

---
+++ Exercises/The Final Exercise. Command Line Crash Course/Command 12. Removing a File (rm)/syntax.sh
@@ -0,0 +1,13 @@
+cd temp
+ls
+rm uncool.txt
+ls
+rm iamcool.txt
+rm neat.txt
+rm thefourthfile.txt
+ls
+cp -r something newplace
+rm something/awesome.txt
+rmdir something
+rm -r newplace
+ls
---
+++ Exercises/The Final Exercise. Command Line Crash Course/Command 4. List Directory (ls)/syntax.sh
@@ -0,0 +1,20 @@
+cd temp
+ls
+cd stuff
+ls
+cd things
+ls
+cd orange
+ls
+cd apple
+ls
+cd pear
+ls
+cd grape
+ls
+cd ..
+ls
+cd ../../../
+ls
+cd ../../
+ls
