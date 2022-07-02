6987ca750cf4e8c27e087689f67d4ccfea24ddf2
Nayef Alkhaldi <100409462+NayefAlkhaldi@users.noreply.github.com>
2022-07-02 20:02:02 +0300

    Done!

--- Exercises/Exercise 52. The Start of Your Web Game/Your Final Exam/data/data.py
+++ Exercises/Exercise 52. The Start of Your Web Game/Your Final Exam/data/data.py
@@ -1,5 +1,5 @@
-users_number = 15
+users_number = 0
 
-system_logins = {'username1': 'test', 'password1': 'test123', 'username2': 'foobar', 'password2': 'foobar', 'username3': 'nayef7', 'password3': 'nayef7', 'username13': 'A123', 'password13': 'A12345', 'username14': 'python', 'password14': 'python', 'username15': 'BBBBBB', 'password15': 'BBBBB'}
+system_logins = {}
 
-system_emails = {'Nayef': 'nayeffalkhaldi@gmail.com', 'test': 'test@test.com', 'foobar': 'foo@bar.com', 'nayef7': 'nayef_f_alkhaldi1538@moe.bh', 'A123': 'A@A.com', 'python': 'pyducation@gmail.com', 'BBBBBB': 'B@B.com'}
+system_emails = {}
