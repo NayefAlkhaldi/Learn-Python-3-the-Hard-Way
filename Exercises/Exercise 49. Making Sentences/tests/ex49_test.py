c8e37ec766f20c53c4c3a2225534b9ae091949d0
Nayef Alkhaldi <100409462+NayefAlkhaldi@users.noreply.github.com>
2026-05-01 19:27:00 +0300

    Add files via upload

---
+++ Exercises/Exercise 49. Making Sentences/tests/ex49_test.py
@@ -0,0 +1,48 @@
+import lexicon
+from parser import sentence
+
+def test_directions():
+    assert lexicon.scan("north") == [('direction', 'north')]
+    result = lexicon.scan("north south east")
+    assert result == [('direction', 'north'),
+                      ('direction', 'south'),
+                      ('direction', 'east')]
+    
+def test_verbs():
+    assert lexicon.scan("go"), [('verb', 'go')]
+    result = lexicon.scan("go kill eat")
+    assert result, [('verb', 'go'),
+                    ('verb', 'kill'),
+                    ('verb', 'eat')]
+
+def test_stops():
+    assert lexicon.scan("the"), [('stop', 'the')]
+    result = lexicon.scan("the in of")
+    assert result, [('stop', 'the'),
+                    ('stop', 'in'),
+                    ('verb', 'eat')]
+
+def test_nouns():
+    assert lexicon.scan("bear"), [('noun', 'bear')]
+    result = lexicon.scan("bear princess")
+    assert result == [('noun', 'bear'),
+                      ('noun', 'princess')]
+
+def test_numbers():
+    assert lexicon.scan("1234") == [('number', 1234)]
+
+def test_errors():
+    assert lexicon.scan("ASDFADFASDF") == [('error', 'ASDFADFASDF')]
+    result = lexicon.scan("bear IAS princess")
+    assert result == [('noun', 'bear'),
+                      ('error', 'IAS'),
+                      ('noun', 'princess')]
+
+def test_spaces():
+    assert lexicon.scan(' PRINCESS     bear') == [('noun', 'PRINCESS'),
+                                                  ('noun', 'bear')]
+
+def test_parser():
+    text = sentence("go left")
+    text.cut()
+    assert text.show() == "player go left"
