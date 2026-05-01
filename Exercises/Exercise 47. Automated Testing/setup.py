7273c53aa9075bf1fbb6449c4e5883340de439f1
Nayef Alkhaldi <100409462+NayefAlkhaldi@users.noreply.github.com>
2026-05-01 19:31:10 +0300

    Add files via upload

---
+++ Exercises/Exercise 47. Automated Testing/setup.py
@@ -0,0 +1,19 @@
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+
+config = {
+	'description': 'Exercise 47. Automated Testing',
+	'author': 'Nayef',
+	'url': '',
+	'download_url': '',
+	'author_email': 'nayeffalkhaldi@gmail.com',
+	'version': '0.1',
+	'install_requires': ['pytest'],
+	'packages': ['NAME'],
+	'scripts': [''],
+	'name': 'ex47'
+}
+
+setup(**config)
